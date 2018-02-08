c3aeb886c65c37eda52387fa154cc2ede026fa9c
skwarte <skwarte@purdue.edu>
2018-02-08 17:38:25 -0500

    trying to init db

--- app/src/main/java/com/example/team22cs407/groceryassistant/MainActivity.java
+++ app/src/main/java/com/example/team22cs407/groceryassistant/MainActivity.java
@@ -8,11 +8,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.database.sqlite.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    SQLiteDatabase db;
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -46,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        db = openOrCreateDatabase("grocery", MODE_PRIVATE, null);
     }
 
 }
