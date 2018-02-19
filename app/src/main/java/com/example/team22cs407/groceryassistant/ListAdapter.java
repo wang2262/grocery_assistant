5eea9949e19cb0719df304b9cdc598619b2a88a0
Yuanyuan Ji <ji79@purdue.edu>
2018-02-19 14:24:43 -0500

    adding popup menu for items in recycleView

--- app/src/main/java/com/example/team22cs407/groceryassistant/ListAdapter.java
+++ app/src/main/java/com/example/team22cs407/groceryassistant/ListAdapter.java
@@ -1,27 +1,62 @@
 package com.example.team22cs407.groceryassistant;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by SignInSon on 2/17/18.
  */
 
 public class ListAdapter extends RecyclerView.Adapter {
-
+    private Context mContext;
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_row, parent, false);
         return new ListViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
         ((ListViewHolder) holder).bindView(position);
+        ((ListViewHolder) holder).listLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, OurData.title[position], Toast.LENGTH_SHORT).show();
+                showPopupMenu(view, position);
+            }
+        });
+    }
+
+    public void showPopupMenu(View view, int position){
+        PopupMenu popupMenu = new PopupMenu(mContext, view);
+        popupMenu.getMenuInflater().inflate(R.menu.action, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.modify:
+                        Log.d("in OnMenuItemClick", "I am in modify");
+                        return true;
+                    case R.id.delete:
+                        Log.d("in OnMenuItemClick", "I am in delete");
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
     }
 
     @Override
@@ -31,11 +66,24 @@ public class ListAdapter extends RecyclerView.Adapter {
 
     private class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView mItemText;
+        private LinearLayout listLayout;
 
-        public  ListViewHolder(View itemView) {
+        public  ListViewHolder(final View itemView) {
             super(itemView);
             mItemText = (TextView) itemView.findViewById(R.id.itemText);
-            itemView.setOnClickListener(this);
+            //itemView.setOnClickListener(this);
+
+            listLayout = (LinearLayout) itemView.findViewById(R.id.list_layout);
+            /*
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view){
+                    int position = getAdapterPosition();
+                    Toast.makeText(itemView.getContext(), OurData.title[position], Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            */
 
         }
 
